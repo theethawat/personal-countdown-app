f3c84c83310273df385351ca1dfb902ba1e170bc
Tin Theethawat Savastham <theethawat.s@outlook.com>
2025-04-03 03:58:47 +0700

    🐛 Fix bug cannot build

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,27 +1,31 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+require("../backend/index.js");
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.cjs'),
-            contextIsolation: true, // Important for security
-            enableRemoteModule: false, // Disable remote module for security reasons
-            nodeIntegration: false // Do not enable nodeIntegration for security reasons
-        }
-    });
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.cjs"),
+      contextIsolation: true, // Important for security
+      enableRemoteModule: false, // Disable remote module for security reasons
+      nodeIntegration: false, // Do not enable nodeIntegration for security reasons
+    },
+  });
 
-    // Load the local web server URL
-    mainWindow.loadURL('http://localhost:5173');
-
+  // Load the local web server URL
+  // Dev
+  // mainWindow.loadURL('http://localhost:5173');
+  // Production
+  mainWindow.loadURL(
+    `file://${path.join(__dirname, "../frontend/dist/index.html")}`
+  );
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
